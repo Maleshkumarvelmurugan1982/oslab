dbf64bd361208d0cdccc2c22cca6b6e2c0f2c2ca
V.Maleshkumar <v.maleshkumar26032006@gmail.com>
2025-07-26 06:31:20 +0530

    Update producer.c

--- EX3/1/producer.c
+++ EX3/1/producer.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
+#include <stdlib.h>
 
 int main() {
     key_t key = 1234;
     int shmid = shmget(key, 100, 0666 | IPC_CREAT);
+    if (shmid < 0) {
+        perror("shmget failed");
+        exit(1);
+    }
+
     char *data = (char *) shmat(shmid, NULL, 0);
+    if (data == (char *) -1) {
+        perror("shmat failed");
+        exit(1);
+    }
 
     // Write message to shared memory
-    printf("Enter message: ");
-    scanf("%s", data);
+    printf("Enter a single word: ");
+    scanf("%99s", data);  // reads only one word
 
-    // Detach
+    // Detach from shared memory
     shmdt(data);
 
     return 0;
 }
+
